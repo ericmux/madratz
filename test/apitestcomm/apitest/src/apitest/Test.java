dd8bd75b9cd4170521f5cc958cfd9ca3689e81f7
Mauro <maurobrito@hotmail.com>
2015-04-01 22:04:28 -0300

    Teste comunicação rpc node Java

---
+++ test/apitestcomm/apitest/src/apitest/Test.java
@@ -0,0 +1,9 @@
+package apitest;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Foo foo = new Foo();
+	}
+
+}
---
+++ test/apitestcomm/apitest/src/apitest/client.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+var DNode = require('dnode');
+
+var client = DNode.connect(6060, function (remote) {
+  remote['moo'](function (x) {
+    console.log(x);
+  });
+});
+
