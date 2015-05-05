7d898fa18e2578b578fc41856a777899b1406e97
Samuel Moreira Timbó <samuelmtimbo@gmail.com>
2015-05-05 16:34:23 -0300

    Got to get player movement's coordinates from text file

---
+++ client/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+using System.IO;
+
+public class PlayerScript : MonoBehaviour {
+	private static int MAX = 200;
+
+	public string fileSource;
+	private float[] xArray = new float[MAX];
+	private float[] zArray = new float[MAX];
+
+	// Use this for initialization
+	void Start () {
+		LoadFromSource (fileSource);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int tick = TimerController.tick;
+
+		if (tick < xArray.Length && tick < zArray.Length) {
+			transform.position = new Vector3 (xArray [tick], transform.position.y, zArray [tick]);
+		} else {
+			TimerController.tick = 0;
+		}
+	}
+
+	private bool LoadFromSource(string fileSource) {
+		// Debug.Log ("Reading coordinates from " + fileSource);
+		try {
+			StreamReader reader = new StreamReader (fileSource, Encoding.Default);
+
+			string line;
+
+			int counter = 0;
+			using (reader) {
+				do {
+					line = reader.ReadLine();
+					// Debug.Log ("Line read: " + line);
+
+					if (line != null) {
+						string[] entries = line.Split(',');
+						if (entries.Length > 0) {
+							float x = float.Parse(entries[0]);
+							xArray[counter] = x;
+							// Debug.Log ("x = " + x);
+
+							float z = float.Parse(entries[1]);
+							zArray[counter] = z;
+							// Debug.Log ("z = " + z);
+					
+							counter ++;
+						}
+					}
+					
+				} while (line != null && counter < MAX);
+				
+				reader.Close();
+			}
+		} catch (IOException e) {
+			Debug.Log ("IOException: " + e.ToString());
+			return false;
+		}
+
+		return true;
+	}
+}
---
+++ client/Assets/Scripts/TimerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerController : MonoBehaviour {
+	public static int tick;
+
+	// Use this for initialization
+	void Start () {
+		tick = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		tick = tick + 1;
+		Debug.Log (tick);
+	}
+}
