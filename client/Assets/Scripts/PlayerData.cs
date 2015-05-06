091db0f80e5870cd6538fff5025f149fe1652fc7
Samuel Moreira Timbó <samuelmtimbo@gmail.com>
2015-05-06 16:24:58 -0300

    Started to create Lab Scene and to work with player's data

--- client/Assets/Scripts/PlayerData.cs
+++ client/Assets/Scripts/PlayerData.cs
@@ -1,36 +1,57 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+using LitJson;
 
 public class PlayerData {
-
-	// Position
-
-	public Vector3 position;
-
-	// Rotation
-
-	public Quaternion rotation;
-
-	// Stats
-
-	public float HP;
-	public float SP;
+	public static string Name { get; set;}
+	public static List<RatData> Rats { get; set;}
 	
-	public Vector3 Position {
-		get {
-			return position;
-		}
-		set {
-			position = value;
-		}
+
+	public static void setDataFromFile(string fileName) {
+
 	}
 
-	public Quaternion Rotation {
-		get {
-			return rotation;
-		}
-		set {
-			rotation = value;
-		}
+	private void setDataFromJSON(JsonData jsonData) {
+		
 	}
+
+	public static void loadDataFromServer() {
+		string json = @"{
+				""name"": ""Samuel"",
+				""rats"": [
+					{
+						""name"": ""Madmax"",
+						""class"": ""slayer"",
+						""level"": 10
+					}, 
+
+					{
+						""name"": ""Legolas"",
+						""class"": ""shooter"",
+						""level"": 5
+					}
+				]
+		}";
+
+		// JsonMapper.ToObject<PlayerData> (json);
+		JsonData data = JsonMapper.ToObject (json);
+
+		Name = (string) data ["name"];
+		Rats = jsonArrayToRats (data ["rats"]);
+	}
+
+	private static List<RatData> jsonArrayToRats(JsonData data) {
+		List<RatData> rats = new List<RatData>();
+		RatData ratData;
+		for (int i = 0; i < data.Count; i ++) {
+			ratData = new RatData();
+			ratData.Name = (string) data[i]["name"];
+			ratData.Class = (string) data[i]["class"];
+
+			rats.Add(ratData);
+		}
+
+		return rats;
+	}
+
 }
---
+++ client/Assets/Scripts/Ring/BattleDirectorController.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using UnityEngine.UI;
+
+public class BattleDirectorController : MonoBehaviour {
+	public GameObject ratPrefab;
+
+	public enum PlayerNumber
+	{
+		PLAYER_NUMBER_1,
+		PLAYER_NUMBER_2,
+		PLAYER_NUMBER_3,
+		PLAYER_NUMBER_4
+	}
+
+	private PlayerNumber playerNumber = PlayerNumber.PLAYER_NUMBER_1;
+
+	private float loadingTime = 0;
+	public Slider loadingSlider;
+	public Text loadingPercentText;
+
+	// Use this for initialization
+	void Start () {
+		GameObject loadingCanvas = GameObject.FindGameObjectWithTag ("LoadingCanvas");
+
+		List<RatSimulationDataUnit> rat1Data = createFromFile ("Assets/Files/rat_circle");
+		List<RatSimulationDataUnit> rat2Data = createFromFile ("Assets/Files/rat_elastic");
+
+		loadingCanvas.SetActive (false);
+
+		GameObject rat1 = (GameObject) Instantiate (ratPrefab);
+		RatSimulationScript rat1Script = rat1.GetComponent<RatSimulationScript> ();
+		rat1Script.loadData (rat1Data);
+		// rat1.SetActive (false);
+
+		GameObject rat2 = (GameObject) Instantiate (ratPrefab);
+		RatSimulationScript rat2Script = rat2.GetComponent<RatSimulationScript> ();
+		rat2Script.loadData (rat2Data);
+		// rat2.SetActive (false);
+
+		// Playing starts when ticks are counting
+		TimerController.startTick ();
+
+		switch (playerNumber) {
+		case PlayerNumber.PLAYER_NUMBER_1:
+			CameraController.player = rat1;
+
+			break;
+		case PlayerNumber.PLAYER_NUMBER_2:
+			CameraController.player = rat2;
+
+			break;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void downloadFiles() {
+
+	}
+
+	public List<RatSimulationDataUnit> createFromFile(string fileName) {
+		List<RatSimulationDataUnit> ratDataList = new List<RatSimulationDataUnit> ();
+
+		Debug.Log ("Reading coordinates from " + fileName);
+		try {
+			StreamReader reader = new StreamReader (fileName, Encoding.Default);
+			
+			string line;
+
+			int nLines = int.Parse(reader.ReadLine());
+			int lineCounter = 0;
+			using (reader) {
+				do {
+					line = reader.ReadLine();
+					// Debug.Log ("Line read: " + line);
+
+					RatSimulationDataUnit data;
+					if (line != null) {
+						string[] entries = line.Split(',');
+						if (entries.Length > 0) {
+							// Get data from line
+							float x = float.Parse(entries[0]);
+							float z = float.Parse(entries[1]);
+							// Debug.Log ("x = " + x + ", z = " + z);
+
+							// Setup a data unit
+							data = new RatSimulationDataUnit();
+							data.Position = new Vector3(x, 0.5f, z);
+
+							// Add it to data list
+							ratDataList.Add(data);
+							Debug.Log ("data.Position = " + data.Position.ToString());
+						}
+					}
+
+					lineCounter ++;
+	
+					loadingTime = lineCounter / nLines;
+					loadingSlider.value = loadingTime;
+					loadingPercentText.text = ((int) loadingTime) + " %%";
+					
+				} while (line != null);
+				
+				reader.Close();
+			}
+		} catch (IOException e) {
+			Debug.Log ("IOException: " + e.ToString());
+		}
+
+		return ratDataList;
+	}
+}
