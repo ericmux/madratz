0a26c4d9c243110479636b17b88ef8326a5eeb78
Victor Elias <victorgelias@gmail.com>
2015-05-27 16:34:45 -0300

    Generating protos to a separate module instead of inside physics
    Also modified the Java proto generator script to change permissions of generated
    classes to read-only, as well as deleting previous files before generating the
    new ones. Source folder is also marked as generated so IntelliJ will complain
    even if read-only flag is overridden.

--- common/makep_java.sh
+++ common/makep_java.sh
@@ -6,4 +6,12 @@ do
 	files+=" "$proto
 done
 
-protoc --java_out=../server/physics/src $files
+if [ -e ../server/protos/src ]
+then
+	chmod -R u+w ../server/protos/src
+	rm -rf ../server/protos/src
+fi
+mkdir ../server/protos/src
+
+protoc --java_out=../server/protos/src $files
+chmod -R -w ../server/protos/src/*
