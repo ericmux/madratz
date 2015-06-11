4665d39083c732c1a13fc9cb4b96501743156a18
Victor Elias <victorgelias@gmail.com>
2015-06-11 07:06:31 -0300

    Restoring client game assets deleted on previous commit
    It's already on repo, deleting it won't save any size :/
    Will add proper .gitignore soon.

---
+++ client/Assets/Scripts/RatData.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RatData {
+	public string Name { get; set; }
+	public string Class { get; set; }
+}
---
+++ client/Assets/Scripts/TimerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerController : MonoBehaviour {
+	public static int tick;
+
+	private static bool startedTick = false;
+
+	// Use this for initialization
+	void Start () {
+		tick = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (startedTick) tick = tick + 1;
+		// Debug.Log (tick);
+	}
+
+	public static void startTick () {
+		TimerController.startedTick = true;
+	}
+}
