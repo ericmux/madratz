1ef94b1e3cd3cfc6b279cf5886ff3535f281bb11
Eric Muxagata <ericgmconrado@gmail.com>
2015-07-05 08:06:03 -0300

    Added MatchMaking Screens.

--- client/TrueMenu/MenuMadRatz/Assets/GlobalVariables.cs
+++ client/TrueMenu/MenuMadRatz/Assets/GlobalVariables.cs
@@ -3,6 +3,36 @@ using System.Collections;
 
 public class GlobalVariables : MonoBehaviour {
 
+
 	public string id;
+
+	public string username;
+	public string password;
+
+
+	private static GlobalVariables _instance;
+	
+	public static GlobalVariables instance {
+		get {
+			if(_instance == null){
+				_instance = GameObject.FindObjectsOfType<GlobalVariables>()[0];
+			}
+
+			_instance.username = "Muxagata";
+			_instance.password = "a1b2c3d4";
+
+			return _instance;
+		}
+	
+	}
+	
+	void Awake(){
+		if(_instance == null)
+		{
+			_instance = this;
+			DontDestroyOnLoad(this);
+		}
+		else if(this != _instance) Destroy(this.gameObject);
+	}
 
 }
---
+++ client/TrueMenu/MenuMadRatz/Assets/Scripts/MatchMakingAdvSearchScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Linq;
+
+public class MatchMakingAdvSearchScript : MonoBehaviour {
+
+	public GlobalVariables _globals;
+	public Text[] textFields;
+	
+	
+	void Start () {
+		textFields = GetComponentsInChildren<Text> ();
+		_globals = GlobalVariables.instance;
+
+		if (!gameObject.activeSelf) return;
+
+		if (textFields.Length > 3) {
+			textFields = textFields.Skip(3).ToArray();
+		}
+
+		StartCoroutine (searchAdversaries ());
+	}
+
+	private IEnumerator searchAdversaries(){
+		int i = 0;
+		while (true) {
+			foreach (Text t in textFields){
+				t.text = string.Concat("Carregando", string.Join ("", Enumerable.Repeat(".",i).ToArray()));
+			}
+			if(i++ == 3) i=0;
+
+			yield return new WaitForSeconds(.5f);
+		}
+	}
+
+}
